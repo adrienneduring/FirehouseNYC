586ad9202def0f67b4d51f37f6913e17ee729408
Allison During <allisonduring@Allison-Durings-MacBook-Pro.local>
2013-10-19 12:06:36 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firehose::Application.config.secret_token = '77bbe653d4513e183a93c93064102d684e3547be9a30b803f05d92f2f8182be0e98b24bc9d41da8cd5b36a78eb6189a535ea6177323dc996119c9d17efab9efa'
